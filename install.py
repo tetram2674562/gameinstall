e8f471aeba9ec3b4f3e807edfc7c745af8738d30
tetram2674562 <54166742+tetram2674562@users.noreply.github.com>
2022-02-16 19:39:57 +0100

    ajout de quelque lignes inutiles :joy:

--- install.py
+++ install.py
@@ -6,4 +6,12 @@ print ("Quel logiciel voulez-vous installez?")
 print ("1) Terraria")
 install = (input("Veuillez écrire le chiffre du logiciel choisi"))
 if install== 1 :
-    os.system(cmd /c '%USERPROFILE%\AppData\Local\MEGAcmd\MEGAclient.exe --get  ')
+        url = 'https://mega.nz/file/pSxG1QzC#CdIFLo3IkQRa1WNtt29wXzEMGq48eZglEsNdbAGHYi0'
+        exec = 'Terraria.exe'
+        dir = '\Terraria'
+os.system("cmd /c %USERPROFILE%\AppData\Local\MEGAcmd\MEGAclient.exe get ",url)
+os.system("cmd /c mkdir",dir)
+os.system("start ",exec," /D=%APPDATA%\gameinstall\Games",dir)
+
+
+# à faire bah mon gars faut que tu crées une nouvelle branche github et que tu ajoutes unscript qui télécharge et install automatiquement git en protable edition bah voilà mec jpeux plus r pour toi et ajoute ce script sur le github... :)
--- main.py
+++ main.py
@@ -18,9 +18,11 @@ def menu():
             print('installation')
             os.system("cmd /c mkdir %APPDATA%\gameinstall")
             os.system("cmd /c mkdir %APPDATA%\gameinstall\dependancy")
+            os.system("cmd /c mkdir %APPDATA%\gameinstall\Games")
             os.system("cmd /c curl --url https://mega.nz/MEGAcmdSetup32.exe -O %APPDATA%\gameinstall\dependancy\MEGAcmdSetup32.exe")
             os.system("cmd /c move MEGAcmdSetup32.exe %APPDATA%\gameinstall\dependancy\ ")
             os.system("cmd /c start %APPDATA%\gameinstall\dependancy\MEGAcmdSetup32.exe /S  ")
+            os.system("cmd /c del ")
             print('terminer!')
             menu()
               #  est la location de megacmd %USERPROFILE%\AppData\Local\MEGAcmd
